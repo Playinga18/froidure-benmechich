b73a2dd6a88e147b0c82666a5cc3221916779d4f
Playinga18 <91030442+Playinga18@users.noreply.github.com>
2022-11-24 22:46:08 +0100

    parser 1.2

--- src/main/java/fr/uge/clone/parser/Parser.java
+++ src/main/java/fr/uge/clone/parser/Parser.java
@@ -1,29 +1,73 @@
 package fr.uge.clone.parser;
 
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.util.TraceClassVisitor;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class Parser {
-    public static void main(String[] args) throws IOException {
-        var finder = ModuleFinder.of(Path.of("C:\\\\Users\\\\froid\\\\Downloads"));
-        var moduleReference = finder.findAll().stream().findFirst().orElseThrow();
-        try(var reader = moduleReference.open()) {
-            for(var filename: (Iterable<String>) reader.list()::iterator) {
+
+    public static ModuleReference finderArtefacts(String name) {
+        var finder = ModuleFinder.of(Path.of(name));
+        return finder.findAll().stream().findFirst().orElseThrow();
+    }
+
+    public static void OpenArtefacts(ModuleReference module){
+        try(var reader = module.open()) {
+            for (var filename : (Iterable<String>) reader.list()::iterator) {
                 if (!filename.endsWith(".class")) {
                     continue;
                 }
-                try(var inputStream = reader.open(filename).orElseThrow()) {
+                var writer = new StringWriter();
+                try (var inputStream = reader.open(filename).orElseThrow()) {
                     var classReader = new ClassReader(inputStream);
-                    var writer = new StringWriter();
-                    classReader.accept(new Source(writer) {
-                    }, 0);
+                    TraceClassVisitor traceVisitor = new TraceClassVisitor(new PrintWriter(writer));
+                    classReader.accept(traceVisitor, 0);
                 }
-                return;
+                parsing(writer.toString());
             }
+        }catch ( IOException e){
+            throw new RuntimeException(e);
         }
+    }
+
+    public static void Opentest() throws Exception {
+        var writer = new StringWriter();
+        try {
+            ClassReader reader = new ClassReader(new FileInputStream("C:\\test\\Hello.class"));
+            TraceClassVisitor traceVisitor = new TraceClassVisitor(new PrintWriter(writer));
+            reader.accept(traceVisitor, 0);
+        } catch (IOException t) {
+            throw new Exception(t);
+        }
+        parsing(writer.toString());
+        //System.out.println(writer);
+    }
+
+    private static String parseUseless(String str){
+        var pat = Pattern.compile("(//.*)").matcher(str).replaceAll("");
+        pat = Pattern.compile("(L[0-9])").matcher(pat).replaceAll("");
+        pat = Pattern.compile("(?:\\h*\\n){2}").matcher(pat).replaceAll("");
+        return  pat;
+    }
+
+    public static String parsing(String inst){
+        var pat = parseUseless(inst);
+
+        System.out.println(pat);
+        //System.out.println(inst);
+        return inst;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Opentest();
+        //OpenArtefacts(finderArtefacts("C:\\\\Users\\\\froid\\\\Downloads"));
     }
 }
--- src/main/java/fr/uge/clone/parser/Source.java
+++ src/main/java/fr/uge/clone/parser/Source.java
@@ -1,14 +1,5 @@
 package fr.uge.clone.parser;
 
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.util.TraceClassVisitor;
+public class Source {
 
-import java.io.PrintWriter;
-import java.io.Writer;
-
-public class Source extends ClassVisitor {
-    public Source(Writer writer) {
-        super(Opcodes.ASM9, new TraceClassVisitor(new PrintWriter(writer)));
-    }
 }
