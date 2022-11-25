71bb5b9164a76752e6516e4c7f013ccbbd4ebb49
Playinga18 <91030442+Playinga18@users.noreply.github.com>
2022-11-25 18:30:52 +0100

    parser 1.3

--- src/main/java/fr/uge/clone/parser/Parser.java
+++ src/main/java/fr/uge/clone/parser/Parser.java
@@ -14,12 +14,12 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    public static ModuleReference finderArtefacts(String name) {
+    private ModuleReference finderArtefacts(String name) {
         var finder = ModuleFinder.of(Path.of(name));
         return finder.findAll().stream().findFirst().orElseThrow();
     }
 
-    public static void OpenArtefacts(ModuleReference module){
+    private void ParseArtefacts(ModuleReference module){
         try(var reader = module.open()) {
             for (var filename : (Iterable<String>) reader.list()::iterator) {
                 if (!filename.endsWith(".class")) {
@@ -31,43 +31,49 @@ public class Parser {
                     TraceClassVisitor traceVisitor = new TraceClassVisitor(new PrintWriter(writer));
                     classReader.accept(traceVisitor, 0);
                 }
-                parsing(writer.toString());
+                var instClass = parsingClass(writer.toString());
             }
         }catch ( IOException e){
             throw new RuntimeException(e);
         }
     }
 
-    public static void Opentest() throws Exception {
+    private void Opentest() throws Exception {
         var writer = new StringWriter();
         try {
-            ClassReader reader = new ClassReader(new FileInputStream("C:\\test\\Hello.class"));
+            ClassReader reader = new ClassReader(new FileInputStream("C:\\test\\Toy.class"));
             TraceClassVisitor traceVisitor = new TraceClassVisitor(new PrintWriter(writer));
             reader.accept(traceVisitor, 0);
         } catch (IOException t) {
             throw new Exception(t);
         }
-        parsing(writer.toString());
+        var instClass = parsingClass(writer.toString());
         //System.out.println(writer);
     }
 
-    private static String parseUseless(String str){
+    private String parseUselessStuff(String str){
         var pat = Pattern.compile("(//.*)").matcher(str).replaceAll("");
-        pat = Pattern.compile("(L[0-9])").matcher(pat).replaceAll("");
-        pat = Pattern.compile("(?:\\h*\\n){2}").matcher(pat).replaceAll("");
+        //pat = Pattern.compile("(L\\d).*").matcher(pat).replaceAll("");
+        //pat = Pattern.compile("\\(.*\\)[A-Z]").matcher(pat).replaceAll("");
+        pat = Pattern.compile("(\\S*\\n){2}").matcher(pat).replaceAll("");
         return  pat;
     }
 
-    public static String parsing(String inst){
-        var pat = parseUseless(inst);
+    private String parsingClass(String inst){
+        var pat = parseUselessStuff(inst);
 
         System.out.println(pat);
         //System.out.println(inst);
         return inst;
     }
 
+    public void parse(String name) throws Exception {
+        //Opentest();
+        ParseArtefacts(finderArtefacts(name));
+    }
+
     public static void main(String[] args) throws Exception {
-        Opentest();
-        //OpenArtefacts(finderArtefacts("C:\\\\Users\\\\froid\\\\Downloads"));
+        var parser = new Parser();
+        parser.parse("C:\\\\Users\\\\froid\\\\Downloads");
     }
 }
