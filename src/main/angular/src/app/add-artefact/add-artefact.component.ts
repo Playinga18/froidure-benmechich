970214251a91a26314f0ff82e0383f037b5e0192
Playinga18 <91030442+Playinga18@users.noreply.github.com>
2022-12-30 01:38:56 +0100

    ajout front end - design

---
+++ src/main/angular/src/app/add-artefact/add-artefact.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-artefact',
+  templateUrl: './add-artefact.component.html',
+  styleUrls: ['./add-artefact.component.css']
+})
+export class AddArtefactComponent {
+}
--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { AddArtefactComponent } from './add-artefact/add-artefact.component';
+import { ListScoreComponent } from './list-score/list-score.component';
 import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
+    AddArtefactComponent,
+    ListScoreComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
