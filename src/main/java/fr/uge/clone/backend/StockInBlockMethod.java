a79b332c57b2b728b013383dc03a7fdc96601191
Yaskow <a.yas44@yahoo.fr>
2022-12-31 02:42:04 +0100

    Class for add Block in db

--- src/main/java/fr/uge/clone/backend/StockInBlockMethod.java
+++ src/main/java/fr/uge/clone/backend/StockInBlockMethod.java
@@ -1,18 +1,27 @@
 package fr.uge.clone.backend;
 
+import fr.uge.clone.model.Artefact;
+import fr.uge.clone.model.Block;
 import fr.uge.clone.repository.BlockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Objects;
 
 @Service
 public class StockInBlockMethod {
-
     private final BlockRepository blockRepository;
 
     public StockInBlockMethod(BlockRepository blockRepository){
         this.blockRepository = blockRepository;
     }
 
+    public Block storeBlock(Artefact artefact) throws Exception {
+        Objects.requireNonNull(artefact);
+        var artefactFile = new ArtefactFile(artefact.getNumVersion());
+        artefactFile.open();
+        var artefactHash = artefactFile.IndexArtefact();
+        return null/*blockRepository.save(artefactHash, artefact.getNumVersion(),)*/;
+    }
 
 }
